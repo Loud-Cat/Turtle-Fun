5c2bb14e3892517ff90bd7245ee41e7e5d23a681
Edison Ditto <73842360+MedisonMan@users.noreply.github.com>
2022-01-05 19:28:08 -0600

    Create hypocycloid.py

---
+++ Circles/hypocycloid.py
@@ -0,0 +1,44 @@
+import turtle
+
+# Setup screen for animation
+turtle.tracer(0)
+
+# Initiate and hide turtle
+t = turtle.Turtle()
+t.hideturtle()
+t.pensize(4)
+
+def home(rad):
+    """Clear screen and set position at the bottom of a cardioid (leaves pen up)"""
+    t.clear()
+    t.penup()
+    t.goto(0, -rad)
+    t.setheading(0)
+    t.pendown()
+
+def loop(rad):
+    points = []
+    for deg in range(360 + 1):
+        home(rad)
+        t.circle(rad)
+
+        t.penup()
+        t.circle(rad, deg)
+        t.pendown()
+
+        t.circle(rad / 6)
+        t.circle(rad / 6, -6*deg - 180)
+        points.append( t.pos() )
+        t.dot(10)
+
+        t.color("red")
+        t.penup()
+        t.goto(*points[0])
+        t.pendown()
+        for x,y in points:
+            t.goto(x,y)
+        t.color("black")
+
+        turtle.update()
+
+loop(200)
